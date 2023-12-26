25cabf736bd5de3cf938e5a3789116d16cbdc741
Oussama_Achour <oachour08@gmail.com>
2023-12-26 22:18:57 +0100

    Add : Consul module

---
+++ module-consul/vars.tf
@@ -0,0 +1,12 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "~/.ssh/aws/aws"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "~/.ssh/aws/aws.pub"
+}
+
